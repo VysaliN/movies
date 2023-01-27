86fd9cbe9fef5b216269184d8cc5b60ee269f6b7
Vysali Natarajan Jayaraman <vjnatarajan@AD.MSYSTECHNOLOGIES.COM>
2023-01-27 20:31:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Movies from "./Movies";
+import "./Movies.css";
+
+const App = () => {
+  return (
+    <div>
+      <Movies />
+    </div>
+  );
+};
+
+export default App;
